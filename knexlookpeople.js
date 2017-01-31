df0b582602962fe297a6ddd977a752c36d997a38
Kapish <kapish.k.1@gmail.com>
2017-01-31 15:01:57 -0500

    added knex to insert tables

--- knexlookpeople.js
+++ knexlookpeople.js
@@ -1,4 +1,15 @@
-const pg = require('pg');
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user: "uzwpoiyxwqnsuw",
+    password: "a2fcfba3ddd4397fc9feff24cb583d2b1af86db434caa885e690b0b290605331",
+    database: "d84occivr3ih92",
+    hostname: "ec2-54-225-67-3.compute-1.amazonaws.com",
+    port: 5432,
+    ssl: true
+  }
+});
+
 const settings = require('./settings');
 const data = require('./data_access')
 
@@ -18,5 +29,4 @@ data.query(q, [process.argv[2]], (err, result) => {
   console.log(`- ${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born '${born.toISOString().split('T')[0]}'`)
   process.exit();
 });
-
 
